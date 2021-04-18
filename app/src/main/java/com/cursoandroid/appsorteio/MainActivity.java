9d891b7b87c86e77fa1dcdb1a0e1f473071c2955
Matheus Alves <matheusalves.bmd@gmail.com>
2021-04-18 01:38:52 -0300

    Implement the 'lottery' logic

--- app/src/main/java/com/cursoandroid/appsorteio/MainActivity.java
+++ app/src/main/java/com/cursoandroid/appsorteio/MainActivity.java
@@ -3,12 +3,24 @@ package com.cursoandroid.appsorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random rand = new Random();
+    int n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view) {
+        n = rand.nextInt(11);
+        TextView resultado = findViewById(R.id.resultado);
+        resultado.setText("O número sorteado: " + n);
     }
 }
